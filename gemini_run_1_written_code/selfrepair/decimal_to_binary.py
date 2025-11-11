dddbcd9aecf25f33da9964c4d994c219c11648e2
natan <72627669+natkai2040@users.noreply.github.com>
2025-11-10 22:52:03 -0500

    Assignment 2

--- gemini_run_1_written_code/selfrepair/decimal_to_binary.py
+++ gemini_run_1_written_code/selfrepair/decimal_to_binary.py
@@ -10,3 +10,13 @@ def decimal_to_binary(n):
     
     # Repair: Reverse the list of digits before joining them
     return "".join(binary_digits[::-1])
+
+# def decimal_to_binary(n): 
+#     binary_digits = []
+#     while n > 0:
+#         remainder = n % 2
+#         binary_digits.append(str(remainder))
+#         n = n // 2
+    
+#     # Repair: Reverse the list of digits before joining them
+#     return "".join(binary_digits[::-1])
